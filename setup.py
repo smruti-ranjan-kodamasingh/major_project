adcdb7a7601da4bfb19c5950413ee2ee2707d198
smruti-2003 <srsingh2678@gmail.com>
2024-10-13 16:40:48 +0530

    packages installed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="major_project",
+    version="0.0.0",
+    author="Smruti",
+    author_email="srsingh2678@gmail.com",
+    packages=find_packages()
+)
